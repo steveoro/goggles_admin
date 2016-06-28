0244acfc8c02f5ff40923817aeebcb4f741f974e
steveoro <steve.alloro@gmail.com>
2016-06-28 19:08:53 +0200

    added zeus local config

---
+++ custom_plan.rb
@@ -0,0 +1,12 @@
+require 'zeus/rails'
+
+class CustomPlan < Zeus::Rails
+
+  def test(*args)
+    ENV['GUARD_RSPEC_RESULTS_FILE'] = 'tmp/guard_rspec_results.txt' # can be anything matching Guard::RSpec :results_file option in the Guardfile
+    super
+  end
+
+end
+
+Zeus.plan = CustomPlan.new

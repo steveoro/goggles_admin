af15a620c0479669c7db657d45b4c130985b2ff0
Marco Ligabue <leega@leegax.homenet.telecomitalia.it>
2016-11-09 10:12:42 +0100

    Follonica 2014 import and swimmer fix

---
+++ db/diff.new/201611090935prod_ris20150412follonica.00.sql
@@ -0,0 +1,14 @@
+-- Leega
+-- 09/11/2016
+-- Fix known swimmers
+update swimmers set year_of_birth = 1937, is_year_guessed = 0 where id = 7026; -- BATTAGLIA DINA
+update swimmers set year_of_birth = 1954, is_year_guessed = 0 where id = 7136; -- CAVALLINI MARCO
+update swimmers set year_of_birth = 1967, is_year_guessed = 0 where id = 7193; -- GARUTI FABIO
+update swimmers set year_of_birth = 1969, is_year_guessed = 0 where id = 7226; -- LEPORI SIMONE
+update swimmers set year_of_birth = 1991, is_year_guessed = 0 where id = 7255; -- MASSINI NICOLA
+update swimmers set year_of_birth = 1957, is_year_guessed = 0 where id = 7275; -- MONI MARCO
+update swimmers set year_of_birth = 1971, is_year_guessed = 0 where id = 7289; -- NIGIOTTI FULVIO
+update swimmers set year_of_birth = 1972, is_year_guessed = 0 where id = 7310; -- PASSERA LEONARDO
+update swimmers set year_of_birth = 1968, is_year_guessed = 0 where id = 7363; -- TAVIANI MAURO
+update swimmers set year_of_birth = 1974, is_year_guessed = 0 where id = 7364; -- TESI ANDREA
+update swimmers set year_of_birth = 1946, is_year_guessed = 0 where id = 7378; -- TURINI TIBERIO
---
+++ db/diff.new/201611090938prod_ris20150412follonica.12.diff.sql
@@ -0,0 +1,6 @@
+-- *** SQL Diff file for ris20150412follonica.txt ***
+-- Timestamp: 201611090938
+UPDATE `meetings` SET `notes`='Amatori Nuoto Follonica' WHERE (`id`=14309);
+
+
+-- Last completed phase code: 12

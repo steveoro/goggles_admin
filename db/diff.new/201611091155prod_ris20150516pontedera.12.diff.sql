c8b50f21441f11d0dc18d092e88a4b5fe50f810e
Marco Ligabue <leega@leegax.homenet.telecomitalia.it>
2016-11-09 12:16:02 +0100

    Pontedera 2015 import and swimmers fix

---
+++ db/diff.new/201611091155prod_ris20150516pontedera.12.diff.sql
@@ -0,0 +1,6 @@
+-- *** SQL Diff file for ris20150516pontedera.txt ***
+-- Timestamp: 201611091155
+UPDATE `meetings` SET `notes`='DN Pontedera' WHERE (`id`=14220);
+
+
+-- Last completed phase code: 12

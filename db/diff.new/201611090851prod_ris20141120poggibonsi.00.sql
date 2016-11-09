113225c2e914b31d83466a8e140f428bea761a59
Marco Ligabue <leega@leegax.homenet.telecomitalia.it>
2016-11-09 09:20:00 +0100

    Poggibonsi 2014 import and swimmer fix

---
+++ db/diff.new/201611090851prod_ris20141120poggibonsi.00.sql
@@ -0,0 +1,10 @@
+-- Leega
+-- 09/11/2016
+-- Known swimmers fix
+update swimmers set year_of_birth = 1967, is_year_guessed = 0 where id = 7042; -- D'ANIELLO DUCCIO
+update swimmers set year_of_birth = 1986, is_year_guessed = 0 where id = 7935; -- D'ORAZIO STEFANO
+update swimmers set year_of_birth = 1978, is_year_guessed = 0 where id = 7202; -- GIORDANI FRANCESCA
+update swimmers set year_of_birth = 1972, is_year_guessed = 0 where id = 7960; -- MAZZUOLI FABRIZIO
+update swimmers set year_of_birth = 1974, is_year_guessed = 0 where id = 7961; -- MEONI LUCA
+update swimmers set year_of_birth = 1987, is_year_guessed = 0 where id = 7963; -- MOSI AGNESE
+
---
+++ db/diff.new/201611090851prod_ris20141123poggibonsi.12.diff.sql
@@ -0,0 +1,6 @@
+-- *** SQL Diff file for ris20141123poggibonsi.txt ***
+-- Timestamp: 201611090851
+UPDATE `meetings` SET `notes`='Virtus Nuoto Poggibonsi' WHERE (`id`=14245);
+
+
+-- Last completed phase code: 12

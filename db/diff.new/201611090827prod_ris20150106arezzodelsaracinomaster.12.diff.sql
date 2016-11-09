1026ce0d6eba693eb42f295171adfcab4a80f06d
Marco Ligabue <leega@leegax.homenet.telecomitalia.it>
2016-11-09 08:38:15 +0100

    Arezzo 2015 import and swimmer fix

---
+++ db/diff.new/201611090827prod_ris20150106arezzodelsaracinomaster.12.diff.sql
@@ -0,0 +1,9 @@
+-- *** SQL Diff file for ris20150106arezzodelsaracinomaster.txt ***
+-- Timestamp: 201611090827
+INSERT INTO `team_affiliations` (`id`, `number`, `name`, `must_calculate_goggle_cup`, `team_id`, `season_id`, `user_id`, `created_at`, `updated_at`, `is_autofilled`)
+  VALUES (2705, NULL, 'SPORT SHUTTLE - SEL', 0, 621, 142, 1, '2016-11-09 07:27:33', '2016-11-09 07:27:33', 1);
+
+UPDATE `meetings` SET `notes`='CHIMERA NUOTO' WHERE (`id`=14261);
+
+
+-- Last completed phase code: 12

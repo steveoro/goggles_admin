21cb2ae1d198b32957a807cf24bdb4e1d6a2bcab
Marco Ligabue <leega@leegax.homenet.telecomitalia.it>
2016-11-15 21:33:20 +0100

    Calendar 2015 and 2016 fine tuning

---
+++ db/diff.new/201611151928prod_unconfirmed_142_meetings.sql
@@ -0,0 +1,5 @@
+-- Leega
+-- 15/11/2016
+-- Set unconfirmed Meeting degli auguri meeting
+-- update meetings set is_confirmed = True where id between 14201 and 14399;
+update meetings set is_confirmed = False where id = 14203;

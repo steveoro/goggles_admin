50a2d2676b5b49f4bf0828e497e142995a1ff245
Marco Ligabue <leega@leegax.homenet.telecomitalia.it>
2016-11-15 18:30:18 +0100

    Reg Toscana 2015 import and swimmers fix

---
+++ db/diff.new/201611151715prod_ris20150221regtoscana.00.sql
@@ -0,0 +1,6 @@
+-- Leega
+-- 15/11/2016
+-- Fix known swimmers
+update swimmers set year_of_birth = 1991, is_year_guessed = 0 where id = 7027; -- BONACOSCIA NICCOLO'
+update swimmers set year_of_birth = 1984, is_year_guessed = 0 where id = 7140; -- CHELLI DANIELE
+update swimmers set year_of_birth = 1963, is_year_guessed = 0 where id = 7041; -- COSTANZI NAZZARENO
---
+++ db/diff.new/201611151826prod_ris20150221regtoscana.40.sql
@@ -0,0 +1,30 @@
+-- Leega
+-- 15/11/2016
+-- Fix known swimmers
+update swimmers set year_of_birth = 1975, is_year_guessed = 0 where id = 28068; -- GOLIN JEAN
+update swimmers set year_of_birth = 1992, is_year_guessed = 0 where id = 28069; -- VERDUCHI DENNY
+update swimmers set year_of_birth = 1959, is_year_guessed = 0 where id = 28070; -- ALBERTINI ENRICO
+update swimmers set year_of_birth = 1964, is_year_guessed = 0 where id = 28071; -- BELLUCCI CESARE ANIA
+update swimmers set year_of_birth = 1955, is_year_guessed = 0 where id = 28072; -- BIAGIOTTI SANDRO
+update swimmers set year_of_birth = 1973, is_year_guessed = 0 where id = 28073; -- BITOSSI LAURA
+update swimmers set year_of_birth = 1986, is_year_guessed = 0 where id = 28074; -- BORGHESI TIZIANO
+update swimmers set year_of_birth = 1958, is_year_guessed = 0 where id = 28075; -- CAPITANI STEFANO
+update swimmers set year_of_birth = 1971, is_year_guessed = 0 where id = 28076; -- CORTONESI FRANCESCO
+update swimmers set year_of_birth = 1961, is_year_guessed = 0 where id = 28077; -- FUSCO ISMAELE
+update swimmers set year_of_birth = 1988, is_year_guessed = 0 where id = 28078; -- GIANNINI RICCARDO
+update swimmers set year_of_birth = 1971, is_year_guessed = 0 where id = 28079; -- GIUNTOLI MARCELLO
+update swimmers set year_of_birth = 1987, is_year_guessed = 0 where id = 28080; -- LENZI MATTEO
+update swimmers set year_of_birth = 1983, is_year_guessed = 0 where id = 28081; -- LOCONTE GRAZIANO
+update swimmers set year_of_birth = 1988, is_year_guessed = 0 where id = 28082; -- MAIONCHI RANIERI
+update swimmers set year_of_birth = 1978, is_year_guessed = 0 where id = 28083; -- MANZINI BARBARA
+update swimmers set year_of_birth = 1992, is_year_guessed = 0 where id = 28084; -- MARTELLI SARA
+update swimmers set year_of_birth = 1972, is_year_guessed = 0 where id = 28085; -- MASSAI LAPO
+update swimmers set year_of_birth = 1983, is_year_guessed = 0 where id = 28086; -- MESCHINI GIANFRANCO
+update swimmers set year_of_birth = 1972, is_year_guessed = 0 where id = 28087; -- MOSCATO ALESSIO
+update swimmers set year_of_birth = 1967, is_year_guessed = 0 where id = 28088; -- PALMOLUNGO MARCO
+update swimmers set year_of_birth = 1988, is_year_guessed = 0 where id = 28089; -- PAOLI IRENE
+update swimmers set year_of_birth = 1995, is_year_guessed = 0 where id = 28090; -- REITANO GIANMARCO
+update swimmers set year_of_birth = 1981, is_year_guessed = 0 where id = 28091; -- SALVADORI SILVIA
+update swimmers set year_of_birth = 1974, is_year_guessed = 0 where id = 28092; -- SARACINO GABRIELE
+update swimmers set year_of_birth = 1988, is_year_guessed = 0 where id = 28093; -- SCARAMUZZA TOMMASO
+update swimmers set year_of_birth = 1980, is_year_guessed = 0 where id = 28094; -- SCHINCARIOL ELISABETTA

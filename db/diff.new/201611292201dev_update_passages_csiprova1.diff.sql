d0354f66150480cbfa6d5b877dde548e088bb073
steveoro <steve.alloro@gmail.com>
2016-11-30 00:51:49 +0100

    added dev DB diff for passages inserted by user 39

---
+++ db/diff.new/201611292201dev_update_passages_csiprova1.diff.sql
@@ -0,0 +1,36 @@
+--
+-- PassagesBatchUpdater: recorded from actions by [User: simonedelrio ( ) ID: 39]
+-- 29 November 2016 22:01:38
+-- Begin script
+--
+
+UPDATE `passages`
+  SET `user_id`=39, `minutes`=0, `seconds`=45, `hundreds`=60, `minutes_from_start`=0, `seconds_from_start`=45, `hundreds_from_start`=60
+  WHERE (`id`=5769);
+UPDATE `passages`
+  SET `user_id`=39, `minutes`=0, `seconds`=48, `hundreds`=0, `minutes_from_start`=1, `seconds_from_start`=33, `hundreds_from_start`=60
+  WHERE (`id`=5770);
+UPDATE `passages`
+  SET `user_id`=39, `minutes`=0, `seconds`=52, `hundreds`=0, `minutes_from_start`=2, `seconds_from_start`=25, `hundreds_from_start`=60
+  WHERE (`id`=5771);
+UPDATE `passages`
+  SET `user_id`=39, `minutes`=0, `seconds`=52, `hundreds`=38, `minutes_from_start`=3, `seconds_from_start`=17, `hundreds_from_start`=98
+  WHERE (`id`=5772);
+UPDATE `passages`
+  SET `user_id`=39, `minutes`=0, `seconds`=52, `hundreds`=9, `minutes_from_start`=4, `seconds_from_start`=10, `hundreds_from_start`=7
+  WHERE (`id`=5773);
+UPDATE `passages`
+  SET `user_id`=39, `minutes`=0, `seconds`=52, `hundreds`=53, `minutes_from_start`=5, `seconds_from_start`=2, `hundreds_from_start`=60
+  WHERE (`id`=5774);
+UPDATE `passages`
+  SET `user_id`=39, `minutes`=0, `seconds`=53, `hundreds`=48, `minutes_from_start`=5, `seconds_from_start`=56, `hundreds_from_start`=8
+  WHERE (`id`=5775);
+UPDATE `passages`
+  SET `user_id`=39, `minutes`=0, `seconds`=54, `hundreds`=22, `minutes_from_start`=6, `seconds_from_start`=50, `hundreds_from_start`=30
+  WHERE (`id`=5776);
+INSERT INTO `passages` (`id`, `minutes`, `seconds`, `hundreds`, `meeting_program_id`, `passage_type_id`, `user_id`, `created_at`, `updated_at`, `reaction_time`, `stroke_cycles`, `not_swam_part_seconds`, `not_swam_part_hundreds`, `not_swam_kick_number`, `breath_number`, `position`, `minutes_from_start`, `seconds_from_start`, `hundreds_from_start`, `is_native_from_start`, `meeting_individual_result_id`, `meeting_entry_id`, `swimmer_id`, `team_id`)
+  VALUES (5777, 0, 41, 4, 83096, 2, 39, '2016-11-29 21:01:39', '2016-11-29 21:01:39', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 41, 4, 0, 474370, NULL, 1537, 35);
+INSERT INTO `passages` (`id`, `minutes`, `seconds`, `hundreds`, `meeting_program_id`, `passage_type_id`, `user_id`, `created_at`, `updated_at`, `reaction_time`, `stroke_cycles`, `not_swam_part_seconds`, `not_swam_part_hundreds`, `not_swam_kick_number`, `breath_number`, `position`, `minutes_from_start`, `seconds_from_start`, `hundreds_from_start`, `is_native_from_start`, `meeting_individual_result_id`, `meeting_entry_id`, `swimmer_id`, `team_id`)
+  VALUES (5778, 0, 46, 15, 83096, 4, 39, '2016-11-29 21:01:39', '2016-11-29 21:01:39', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 87, 19, 0, 474370, NULL, 1537, 35);
+INSERT INTO `passages` (`id`, `minutes`, `seconds`, `hundreds`, `meeting_program_id`, `passage_type_id`, `user_id`, `created_at`, `updated_at`, `reaction_time`, `stroke_cycles`, `not_swam_part_seconds`, `not_swam_part_hundreds`, `not_swam_kick_number`, `breath_number`, `position`, `minutes_from_start`, `seconds_from_start`, `hundreds_from_start`, `is_native_from_start`, `meeting_individual_result_id`, `meeting_entry_id`, `swimmer_id`, `team_id`)
+  VALUES (5779, 4, 54, 71, 83096, 16, 39, '2016-11-29 21:01:39', '2016-11-29 21:01:39', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 6, 21, 90, 0, 474370, NULL, 1537, 35);
---
+++ db/diff.new/201611292203dev_update_passages_csiprova1.diff.sql
@@ -0,0 +1,71 @@
+--
+-- PassagesBatchUpdater: recorded from actions by [User: simonedelrio ( ) ID: 39]
+-- 29 November 2016 22:03:35
+-- Begin script
+--
+
+UPDATE `passages`
+  SET `user_id`=39, `minutes`=0, `seconds`=45, `hundreds`=60, `minutes_from_start`=0, `seconds_from_start`=45, `hundreds_from_start`=60
+  WHERE (`id`=5769);
+UPDATE `passages`
+  SET `user_id`=39, `minutes`=0, `seconds`=48, `hundreds`=0, `minutes_from_start`=1, `seconds_from_start`=33, `hundreds_from_start`=60
+  WHERE (`id`=5770);
+UPDATE `passages`
+  SET `user_id`=39, `minutes`=0, `seconds`=52, `hundreds`=0, `minutes_from_start`=2, `seconds_from_start`=25, `hundreds_from_start`=60
+  WHERE (`id`=5771);
+UPDATE `passages`
+  SET `user_id`=39, `minutes`=0, `seconds`=52, `hundreds`=38, `minutes_from_start`=3, `seconds_from_start`=17, `hundreds_from_start`=98
+  WHERE (`id`=5772);
+UPDATE `passages`
+  SET `user_id`=39, `minutes`=0, `seconds`=52, `hundreds`=9, `minutes_from_start`=4, `seconds_from_start`=10, `hundreds_from_start`=7
+  WHERE (`id`=5773);
+UPDATE `passages`
+  SET `user_id`=39, `minutes`=0, `seconds`=52, `hundreds`=53, `minutes_from_start`=5, `seconds_from_start`=2, `hundreds_from_start`=60
+  WHERE (`id`=5774);
+UPDATE `passages`
+  SET `user_id`=39, `minutes`=0, `seconds`=53, `hundreds`=48, `minutes_from_start`=5, `seconds_from_start`=56, `hundreds_from_start`=8
+  WHERE (`id`=5775);
+UPDATE `passages`
+  SET `user_id`=39, `minutes`=0, `seconds`=54, `hundreds`=22, `minutes_from_start`=6, `seconds_from_start`=50, `hundreds_from_start`=30
+  WHERE (`id`=5776);
+UPDATE `passages`
+  SET `user_id`=39, `minutes`=0, `seconds`=41, `hundreds`=4, `minutes_from_start`=0, `seconds_from_start`=41, `hundreds_from_start`=4
+  WHERE (`id`=5777);
+UPDATE `passages`
+  SET `user_id`=39, `minutes`=0, `seconds`=46, `hundreds`=15, `minutes_from_start`=1, `seconds_from_start`=27, `hundreds_from_start`=19
+  WHERE (`id`=5778);
+UPDATE `passages`
+  SET `user_id`=39, `minutes`=4, `seconds`=54, `hundreds`=71, `minutes_from_start`=6, `seconds_from_start`=21, `hundreds_from_start`=90
+  WHERE (`id`=5779);
+INSERT INTO `passages` (`id`, `minutes`, `seconds`, `hundreds`, `meeting_program_id`, `passage_type_id`, `user_id`, `created_at`, `updated_at`, `reaction_time`, `stroke_cycles`, `not_swam_part_seconds`, `not_swam_part_hundreds`, `not_swam_kick_number`, `breath_number`, `position`, `minutes_from_start`, `seconds_from_start`, `hundreds_from_start`, `is_native_from_start`, `meeting_individual_result_id`, `meeting_entry_id`, `swimmer_id`, `team_id`)
+  VALUES (5780, 0, 37, 91, 83097, 2, 39, '2016-11-29 21:03:36', '2016-11-29 21:03:36', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 37, 91, 0, 474372, NULL, 11732, 35);
+INSERT INTO `passages` (`id`, `minutes`, `seconds`, `hundreds`, `meeting_program_id`, `passage_type_id`, `user_id`, `created_at`, `updated_at`, `reaction_time`, `stroke_cycles`, `not_swam_part_seconds`, `not_swam_part_hundreds`, `not_swam_kick_number`, `breath_number`, `position`, `minutes_from_start`, `seconds_from_start`, `hundreds_from_start`, `is_native_from_start`, `meeting_individual_result_id`, `meeting_entry_id`, `swimmer_id`, `team_id`)
+  VALUES (5781, 0, 43, 28, 83097, 4, 39, '2016-11-29 21:03:36', '2016-11-29 21:03:36', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 80, 119, 0, 474372, NULL, 11732, 35);
+INSERT INTO `passages` (`id`, `minutes`, `seconds`, `hundreds`, `meeting_program_id`, `passage_type_id`, `user_id`, `created_at`, `updated_at`, `reaction_time`, `stroke_cycles`, `not_swam_part_seconds`, `not_swam_part_hundreds`, `not_swam_kick_number`, `breath_number`, `position`, `minutes_from_start`, `seconds_from_start`, `hundreds_from_start`, `is_native_from_start`, `meeting_individual_result_id`, `meeting_entry_id`, `swimmer_id`, `team_id`)
+  VALUES (5782, 0, 46, 46, 83097, 6, 39, '2016-11-29 21:03:37', '2016-11-29 21:03:37', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 1, 67, 65, 0, 474372, NULL, 11732, 35);
+INSERT INTO `passages` (`id`, `minutes`, `seconds`, `hundreds`, `meeting_program_id`, `passage_type_id`, `user_id`, `created_at`, `updated_at`, `reaction_time`, `stroke_cycles`, `not_swam_part_seconds`, `not_swam_part_hundreds`, `not_swam_kick_number`, `breath_number`, `position`, `minutes_from_start`, `seconds_from_start`, `hundreds_from_start`, `is_native_from_start`, `meeting_individual_result_id`, `meeting_entry_id`, `swimmer_id`, `team_id`)
+  VALUES (5783, 0, 48, 93, 83097, 8, 39, '2016-11-29 21:03:37', '2016-11-29 21:03:37', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 2, 55, 158, 0, 474372, NULL, 11732, 35);
+INSERT INTO `passages` (`id`, `minutes`, `seconds`, `hundreds`, `meeting_program_id`, `passage_type_id`, `user_id`, `created_at`, `updated_at`, `reaction_time`, `stroke_cycles`, `not_swam_part_seconds`, `not_swam_part_hundreds`, `not_swam_kick_number`, `breath_number`, `position`, `minutes_from_start`, `seconds_from_start`, `hundreds_from_start`, `is_native_from_start`, `meeting_individual_result_id`, `meeting_entry_id`, `swimmer_id`, `team_id`)
+  VALUES (5784, 0, 48, 38, 83097, 10, 39, '2016-11-29 21:03:37', '2016-11-29 21:03:37', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 2, 104, 96, 0, 474372, NULL, 11732, 35);
+INSERT INTO `passages` (`id`, `minutes`, `seconds`, `hundreds`, `meeting_program_id`, `passage_type_id`, `user_id`, `created_at`, `updated_at`, `reaction_time`, `stroke_cycles`, `not_swam_part_seconds`, `not_swam_part_hundreds`, `not_swam_kick_number`, `breath_number`, `position`, `minutes_from_start`, `seconds_from_start`, `hundreds_from_start`, `is_native_from_start`, `meeting_individual_result_id`, `meeting_entry_id`, `swimmer_id`, `team_id`)
+  VALUES (5785, 0, 49, 9, 83097, 12, 39, '2016-11-29 21:03:37', '2016-11-29 21:03:37', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 3, 93, 105, 0, 474372, NULL, 11732, 35);
+INSERT INTO `passages` (`id`, `minutes`, `seconds`, `hundreds`, `meeting_program_id`, `passage_type_id`, `user_id`, `created_at`, `updated_at`, `reaction_time`, `stroke_cycles`, `not_swam_part_seconds`, `not_swam_part_hundreds`, `not_swam_kick_number`, `breath_number`, `position`, `minutes_from_start`, `seconds_from_start`, `hundreds_from_start`, `is_native_from_start`, `meeting_individual_result_id`, `meeting_entry_id`, `swimmer_id`, `team_id`)
+  VALUES (5786, 0, 49, 72, 83097, 14, 39, '2016-11-29 21:03:37', '2016-11-29 21:03:37', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 4, 83, 77, 0, 474372, NULL, 11732, 35);
+INSERT INTO `passages` (`id`, `minutes`, `seconds`, `hundreds`, `meeting_program_id`, `passage_type_id`, `user_id`, `created_at`, `updated_at`, `reaction_time`, `stroke_cycles`, `not_swam_part_seconds`, `not_swam_part_hundreds`, `not_swam_kick_number`, `breath_number`, `position`, `minutes_from_start`, `seconds_from_start`, `hundreds_from_start`, `is_native_from_start`, `meeting_individual_result_id`, `meeting_entry_id`, `swimmer_id`, `team_id`)
+  VALUES (5787, 0, 48, 13, 83097, 16, 39, '2016-11-29 21:03:37', '2016-11-29 21:03:37', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 6, 11, 90, 0, 474372, NULL, 11732, 35);
+INSERT INTO `passages` (`id`, `minutes`, `seconds`, `hundreds`, `meeting_program_id`, `passage_type_id`, `user_id`, `created_at`, `updated_at`, `reaction_time`, `stroke_cycles`, `not_swam_part_seconds`, `not_swam_part_hundreds`, `not_swam_kick_number`, `breath_number`, `position`, `minutes_from_start`, `seconds_from_start`, `hundreds_from_start`, `is_native_from_start`, `meeting_individual_result_id`, `meeting_entry_id`, `swimmer_id`, `team_id`)
+  VALUES (5788, 0, 30, 85, 83079, 2, 39, '2016-11-29 21:03:37', '2016-11-29 21:03:37', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 30, 85, 0, 474375, NULL, 1452, 35);
+INSERT INTO `passages` (`id`, `minutes`, `seconds`, `hundreds`, `meeting_program_id`, `passage_type_id`, `user_id`, `created_at`, `updated_at`, `reaction_time`, `stroke_cycles`, `not_swam_part_seconds`, `not_swam_part_hundreds`, `not_swam_kick_number`, `breath_number`, `position`, `minutes_from_start`, `seconds_from_start`, `hundreds_from_start`, `is_native_from_start`, `meeting_individual_result_id`, `meeting_entry_id`, `swimmer_id`, `team_id`)
+  VALUES (5789, 0, 35, 26, 83079, 4, 39, '2016-11-29 21:03:37', '2016-11-29 21:03:37', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 65, 111, 0, 474375, NULL, 1452, 35);
+INSERT INTO `passages` (`id`, `minutes`, `seconds`, `hundreds`, `meeting_program_id`, `passage_type_id`, `user_id`, `created_at`, `updated_at`, `reaction_time`, `stroke_cycles`, `not_swam_part_seconds`, `not_swam_part_hundreds`, `not_swam_kick_number`, `breath_number`, `position`, `minutes_from_start`, `seconds_from_start`, `hundreds_from_start`, `is_native_from_start`, `meeting_individual_result_id`, `meeting_entry_id`, `swimmer_id`, `team_id`)
+  VALUES (5790, 0, 36, 60, 83079, 6, 39, '2016-11-29 21:03:38', '2016-11-29 21:03:38', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 1, 42, 71, 0, 474375, NULL, 1452, 35);
+INSERT INTO `passages` (`id`, `minutes`, `seconds`, `hundreds`, `meeting_program_id`, `passage_type_id`, `user_id`, `created_at`, `updated_at`, `reaction_time`, `stroke_cycles`, `not_swam_part_seconds`, `not_swam_part_hundreds`, `not_swam_kick_number`, `breath_number`, `position`, `minutes_from_start`, `seconds_from_start`, `hundreds_from_start`, `is_native_from_start`, `meeting_individual_result_id`, `meeting_entry_id`, `swimmer_id`, `team_id`)
+  VALUES (5791, 0, 36, 90, 83079, 8, 39, '2016-11-29 21:03:38', '2016-11-29 21:03:38', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 1, 78, 161, 0, 474375, NULL, 1452, 35);
+INSERT INTO `passages` (`id`, `minutes`, `seconds`, `hundreds`, `meeting_program_id`, `passage_type_id`, `user_id`, `created_at`, `updated_at`, `reaction_time`, `stroke_cycles`, `not_swam_part_seconds`, `not_swam_part_hundreds`, `not_swam_kick_number`, `breath_number`, `position`, `minutes_from_start`, `seconds_from_start`, `hundreds_from_start`, `is_native_from_start`, `meeting_individual_result_id`, `meeting_entry_id`, `swimmer_id`, `team_id`)
+  VALUES (5792, 0, 36, 80, 83079, 10, 39, '2016-11-29 21:03:38', '2016-11-29 21:03:38', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 2, 55, 141, 0, 474375, NULL, 1452, 35);
+INSERT INTO `passages` (`id`, `minutes`, `seconds`, `hundreds`, `meeting_program_id`, `passage_type_id`, `user_id`, `created_at`, `updated_at`, `reaction_time`, `stroke_cycles`, `not_swam_part_seconds`, `not_swam_part_hundreds`, `not_swam_kick_number`, `breath_number`, `position`, `minutes_from_start`, `seconds_from_start`, `hundreds_from_start`, `is_native_from_start`, `meeting_individual_result_id`, `meeting_entry_id`, `swimmer_id`, `team_id`)
+  VALUES (5793, 0, 37, 60, 83079, 12, 39, '2016-11-29 21:03:38', '2016-11-29 21:03:38', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 2, 93, 101, 0, 474375, NULL, 1452, 35);
+INSERT INTO `passages` (`id`, `minutes`, `seconds`, `hundreds`, `meeting_program_id`, `passage_type_id`, `user_id`, `created_at`, `updated_at`, `reaction_time`, `stroke_cycles`, `not_swam_part_seconds`, `not_swam_part_hundreds`, `not_swam_kick_number`, `breath_number`, `position`, `minutes_from_start`, `seconds_from_start`, `hundreds_from_start`, `is_native_from_start`, `meeting_individual_result_id`, `meeting_entry_id`, `swimmer_id`, `team_id`)
+  VALUES (5794, 0, 37, 0, 83079, 14, 39, '2016-11-29 21:03:38', '2016-11-29 21:03:38', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 3, 71, 1, 0, 474375, NULL, 1452, 35);
+INSERT INTO `passages` (`id`, `minutes`, `seconds`, `hundreds`, `meeting_program_id`, `passage_type_id`, `user_id`, `created_at`, `updated_at`, `reaction_time`, `stroke_cycles`, `not_swam_part_seconds`, `not_swam_part_hundreds`, `not_swam_kick_number`, `breath_number`, `position`, `minutes_from_start`, `seconds_from_start`, `hundreds_from_start`, `is_native_from_start`, `meeting_individual_result_id`, `meeting_entry_id`, `swimmer_id`, `team_id`)
+  VALUES (5795, 0, 35, 79, 83079, 16, 39, '2016-11-29 21:03:38', '2016-11-29 21:03:38', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 4, 46, 80, 0, 474375, NULL, 1452, 35);

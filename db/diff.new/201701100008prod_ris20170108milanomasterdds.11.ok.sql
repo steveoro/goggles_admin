795afff8351c56acc177493718d4f95c7b65251a
Marco Ligabue <leega@leegax.homenet.telecomitalia.it>
2017-01-10 00:28:44 +0100

    Week end meeting import and regionali program fix

---
+++ db/diff.new/201701100008prod_ris20170108milanomasterdds.11.ok.sql
@@ -0,0 +1,38 @@
+--
+-- *** Suggested SQL actions: ***
+--
+
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+
+
+-- Processing:...'Asptt Marseille':
+
+-- Processing:...'Nuotopiu` Academy Asd':
+-- aliased with: 'NUOTOPIÙ ACADEMY AS' (ID:54)
+INSERT INTO `data_import_team_aliases` (`id`, `created_at`, `updated_at`, `name`, `team_id`)
+  VALUES (987, '2017-01-09 23:10:44', '2017-01-09 23:10:44', 'Nuotopiu` Academy Asd', 54);
+
+
+-- Processing:...'Sport Training 2 Ssd':
+
+COMMIT;
+--
+-- *** Suggested SQL actions: ***
+--
+
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+
+
+-- Processing:...'GHIRARDI DENIS ENRICO' (1978, gender: 1)
+
+-- Processing:...'GIANOLIO ADRIANA ROSA' (1967, gender: 2)
+-- aliased with: 'GIANOLIO ADRIANA' (ID:20445)
+INSERT INTO `data_import_swimmer_aliases` (`id`, `created_at`, `updated_at`, `complete_name`, `swimmer_id`)
+  VALUES (695, '2017-01-09 23:13:34', '2017-01-09 23:13:34', 'GIANOLIO ADRIANA ROSA', 20445);
+
+
+-- Processing:...'LOBINA EDOARDO' (1992, gender: 1)
+
+COMMIT;
---
+++ db/diff.new/201701100008prod_ris20170108milanomasterdds.12.diff.sql
@@ -0,0 +1,6 @@
+-- *** SQL Diff file for ris20170108milanomasterdds.txt ***
+-- Timestamp: 201701100008
+UPDATE `meetings` SET `notes`='DDS' WHERE (`id`=16262);
+
+
+-- Last completed phase code: 12

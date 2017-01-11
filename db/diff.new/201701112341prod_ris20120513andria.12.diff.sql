d5c65d2b4a1a349736c8a5719b0c77b817f106d8
Marco Ligabue <leega@leegax.homenet.telecomitalia.it>
2017-01-11 23:54:42 +0100

    Andria 2012 import

---
+++ db/diff.new/201701112341prod_ris20120513andria.12.diff.sql
@@ -0,0 +1,57 @@
+-- *** SQL Diff file for ris20120513andria.txt ***
+-- Timestamp: 201701112341
+UPDATE `meetings` SET `notes`='13 Maggio 2012\r\nAqua SPORT Planet Andria' WHERE (`id`=11241);
+
+INSERT INTO `time_standards` (`id`, `minutes`, `seconds`, `hundreds`, `season_id`, `gender_type_id`, `pool_type_id`, `event_type_id`, `category_type_id`, `created_at`, `updated_at`)
+  VALUES (9006, 1, 11, 41, 112, 2, 1, 20, 568, '2017-01-11 22:42:44', '2017-01-11 22:42:44');
+
+INSERT INTO `time_standards` (`id`, `minutes`, `seconds`, `hundreds`, `season_id`, `gender_type_id`, `pool_type_id`, `event_type_id`, `category_type_id`, `created_at`, `updated_at`)
+  VALUES (9007, 1, 12, 11, 112, 2, 1, 20, 569, '2017-01-11 22:42:44', '2017-01-11 22:42:44');
+
+INSERT INTO `time_standards` (`id`, `minutes`, `seconds`, `hundreds`, `season_id`, `gender_type_id`, `pool_type_id`, `event_type_id`, `category_type_id`, `created_at`, `updated_at`)
+  VALUES (9008, 1, 14, 22, 112, 2, 1, 20, 570, '2017-01-11 22:42:45', '2017-01-11 22:42:45');
+
+INSERT INTO `time_standards` (`id`, `minutes`, `seconds`, `hundreds`, `season_id`, `gender_type_id`, `pool_type_id`, `event_type_id`, `category_type_id`, `created_at`, `updated_at`)
+  VALUES (9009, 1, 15, 53, 112, 2, 1, 20, 571, '2017-01-11 22:42:45', '2017-01-11 22:42:45');
+
+INSERT INTO `time_standards` (`id`, `minutes`, `seconds`, `hundreds`, `season_id`, `gender_type_id`, `pool_type_id`, `event_type_id`, `category_type_id`, `created_at`, `updated_at`)
+  VALUES (9010, 1, 1, 99, 112, 1, 1, 20, 568, '2017-01-11 22:42:45', '2017-01-11 22:42:45');
+
+INSERT INTO `time_standards` (`id`, `minutes`, `seconds`, `hundreds`, `season_id`, `gender_type_id`, `pool_type_id`, `event_type_id`, `category_type_id`, `created_at`, `updated_at`)
+  VALUES (9011, 1, 1, 88, 112, 1, 1, 20, 569, '2017-01-11 22:42:46', '2017-01-11 22:42:46');
+
+INSERT INTO `time_standards` (`id`, `minutes`, `seconds`, `hundreds`, `season_id`, `gender_type_id`, `pool_type_id`, `event_type_id`, `category_type_id`, `created_at`, `updated_at`)
+  VALUES (9012, 1, 3, 24, 112, 1, 1, 20, 570, '2017-01-11 22:42:46', '2017-01-11 22:42:46');
+
+INSERT INTO `time_standards` (`id`, `minutes`, `seconds`, `hundreds`, `season_id`, `gender_type_id`, `pool_type_id`, `event_type_id`, `category_type_id`, `created_at`, `updated_at`)
+  VALUES (9013, 1, 4, 27, 112, 1, 1, 20, 571, '2017-01-11 22:42:46', '2017-01-11 22:42:46');
+
+INSERT INTO `time_standards` (`id`, `minutes`, `seconds`, `hundreds`, `season_id`, `gender_type_id`, `pool_type_id`, `event_type_id`, `category_type_id`, `created_at`, `updated_at`)
+  VALUES (9014, 1, 6, 62, 112, 1, 1, 20, 572, '2017-01-11 22:42:47', '2017-01-11 22:42:47');
+
+INSERT INTO `time_standards` (`id`, `minutes`, `seconds`, `hundreds`, `season_id`, `gender_type_id`, `pool_type_id`, `event_type_id`, `category_type_id`, `created_at`, `updated_at`)
+  VALUES (9015, 1, 9, 34, 112, 1, 1, 20, 573, '2017-01-11 22:42:47', '2017-01-11 22:42:47');
+
+INSERT INTO `time_standards` (`id`, `minutes`, `seconds`, `hundreds`, `season_id`, `gender_type_id`, `pool_type_id`, `event_type_id`, `category_type_id`, `created_at`, `updated_at`)
+  VALUES (9016, 1, 12, 43, 112, 1, 1, 20, 574, '2017-01-11 22:42:47', '2017-01-11 22:42:47');
+
+INSERT INTO `time_standards` (`id`, `minutes`, `seconds`, `hundreds`, `season_id`, `gender_type_id`, `pool_type_id`, `event_type_id`, `category_type_id`, `created_at`, `updated_at`)
+  VALUES (9017, 1, 24, 78, 112, 1, 1, 20, 577, '2017-01-11 22:42:48', '2017-01-11 22:42:48');
+
+INSERT INTO `time_standards` (`id`, `minutes`, `seconds`, `hundreds`, `season_id`, `gender_type_id`, `pool_type_id`, `event_type_id`, `category_type_id`, `created_at`, `updated_at`)
+  VALUES (9018, 2, 25, 50, 112, 2, 1, 23, 570, '2017-01-11 22:42:53', '2017-01-11 22:42:53');
+
+INSERT INTO `time_standards` (`id`, `minutes`, `seconds`, `hundreds`, `season_id`, `gender_type_id`, `pool_type_id`, `event_type_id`, `category_type_id`, `created_at`, `updated_at`)
+  VALUES (9019, 2, 37, 93, 112, 2, 1, 23, 573, '2017-01-11 22:42:54', '2017-01-11 22:42:54');
+
+INSERT INTO `time_standards` (`id`, `minutes`, `seconds`, `hundreds`, `season_id`, `gender_type_id`, `pool_type_id`, `event_type_id`, `category_type_id`, `created_at`, `updated_at`)
+  VALUES (9020, 2, 45, 9, 112, 1, 1, 4, 578, '2017-01-11 22:43:00', '2017-01-11 22:43:00');
+
+INSERT INTO `time_standards` (`id`, `minutes`, `seconds`, `hundreds`, `season_id`, `gender_type_id`, `pool_type_id`, `event_type_id`, `category_type_id`, `created_at`, `updated_at`)
+  VALUES (9021, 0, 37, 98, 112, 1, 1, 15, 578, '2017-01-11 22:43:03', '2017-01-11 22:43:03');
+
+INSERT INTO `time_standards` (`id`, `minutes`, `seconds`, `hundreds`, `season_id`, `gender_type_id`, `pool_type_id`, `event_type_id`, `category_type_id`, `created_at`, `updated_at`)
+  VALUES (9022, 0, 30, 8, 112, 1, 1, 11, 576, '2017-01-11 22:43:10', '2017-01-11 22:43:10');
+
+
+-- Last completed phase code: 12

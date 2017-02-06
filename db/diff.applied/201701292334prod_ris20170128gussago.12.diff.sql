1afea489ee410d3a99666ce490e70d93fcebed28
steveoro <steve.alloro@gmail.com>
2017-02-06 12:22:20 +0100

    clean up for applied DB diffs

--- db/diff.applied/201701292334prod_ris20170128gussago.12.diff.sql
+++
@@ -1,6 +0,0 @@
--- *** SQL Diff file for ris20170128gussago.txt ***
--- Timestamp: 201701292334
-UPDATE `meetings` SET `notes`='Brescia Acquare` Mafeco' WHERE (`id`=16205);
-
-
--- Last completed phase code: 12

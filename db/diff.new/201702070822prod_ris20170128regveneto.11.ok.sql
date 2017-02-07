4e5f8352f049e1ff85fca0113b9a2e40fd01a817
Marco Ligabue <leega@leegax.lan>
2017-02-07 08:40:26 +0100

    The fucking Regionali veneto import

---
+++ db/diff.new/201702070822prod_ris20170128regveneto.11.ok.sql
@@ -0,0 +1,15 @@
+--
+-- *** Suggested SQL actions: ***
+--
+
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+
+
+-- Processing:...'BONUTTI LORENZO' (1992, gender: 1)
+
+-- Processing:...'SERPELLONI UGO' (1979, gender: 1)
+
+-- Processing:...'ZALTRON ANDREA' (1966, gender: 1)
+
+COMMIT;
---
+++ db/diff.new/201702070822prod_ris20170128regveneto.12.diff.sql
@@ -0,0 +1,6 @@
+-- *** SQL Diff file for ris20170128regveneto.txt ***
+-- Timestamp: 201702070822
+UPDATE `meetings` SET `notes`='28 Gennaio 2017\r\nCR Veneto' WHERE (`id`=16276);
+
+
+-- Last completed phase code: 12

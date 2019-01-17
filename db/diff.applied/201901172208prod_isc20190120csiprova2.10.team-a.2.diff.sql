ec5a416ce132b203a7e5fc7e08b5f466154c0a50
Marco Ligabue <leega@leegax.lan>
2019-01-17 22:16:58 +0100

    CSI Prova 2 startlist

---
+++ db/diff.applied/201901172208prod_isc20190120csiprova2.10.team-a.2.diff.sql
@@ -0,0 +1,30 @@
+-- caller: base_twice_loggable.rb:70 | base_twice_loggable.rb:70 | home_controller.rb:147 | basic_implicit_render.rb:4
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+/*!40101 SET NAMES utf8 */;
+
+--
+
+-- Processing:...'G.S.Guardia Finanza' (ID:1280, season ID: 181):
+-- aliased with: 'Guardia Finanza MO' (ID:1280)
+INSERT INTO `data_import_team_aliases` (`id`, `created_at`, `updated_at`, `name`, `team_id`)
+  VALUES (1405, '2019-01-17 21:09:07', '2019-01-17 21:09:07', 'G.S.Guardia Finanza', 1280);
+
+
+-- Processing:...'NC Azzurra 1991 CSI' (ID:9, season ID: 181):
+-- aliased with: 'N.C. AZZURRA 1991' (ID:9)
+INSERT INTO `data_import_team_aliases` (`id`, `created_at`, `updated_at`, `name`, `team_id`)
+  VALUES (1406, '2019-01-17 21:09:07', '2019-01-17 21:09:07', 'NC Azzurra 1991 CSI', 9);
+
+
+-- Processing:...'Olympia Nuoto Team' (ID:1000, season ID: 181):
+-- aliased with: 'Olympia SSDRL' (ID:1000)
+INSERT INTO `data_import_team_aliases` (`id`, `created_at`, `updated_at`, `name`, `team_id`)
+  VALUES (1407, '2019-01-17 21:09:07', '2019-01-17 21:09:07', 'Olympia Nuoto Team', 1000);
+
+
+--
+COMMIT;
+
---
+++ db/diff.applied/201901172208prod_isc20190120csiprova2.11.swimmer-a.2.diff.sql
@@ -0,0 +1,30 @@
+-- caller: base_twice_loggable.rb:70 | base_twice_loggable.rb:70 | home_controller.rb:298 | basic_implicit_render.rb:4
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+/*!40101 SET NAMES utf8 */;
+
+--
+
+-- Processing:...'CLÒ ANDREA' (1972, gender: 1)
+-- aliased with: 'CLO' ANDREA' (ID:22139)
+INSERT INTO `data_import_swimmer_aliases` (`id`, `created_at`, `updated_at`, `complete_name`, `swimmer_id`)
+  VALUES (1535, '2019-01-17 21:09:15', '2019-01-17 21:09:15', 'CLÒ ANDREA', 22139);
+
+
+-- Processing:...'D`ANDREA SABINA' (2005, gender: 2)
+-- aliased with: 'D'ANDREA SABINA' (ID:39346)
+INSERT INTO `data_import_swimmer_aliases` (`id`, `created_at`, `updated_at`, `complete_name`, `swimmer_id`)
+  VALUES (1536, '2019-01-17 21:09:15', '2019-01-17 21:09:15', 'D`ANDREA SABINA', 39346);
+
+
+-- Processing:...'SIMONAZZI AURORA' (2000, gender: 2)
+-- aliased with: 'SIMIONAZZI AURORA' (ID:33669)
+INSERT INTO `data_import_swimmer_aliases` (`id`, `created_at`, `updated_at`, `complete_name`, `swimmer_id`)
+  VALUES (1537, '2019-01-17 21:09:15', '2019-01-17 21:09:15', 'SIMONAZZI AURORA', 33669);
+
+
+--
+COMMIT;
+

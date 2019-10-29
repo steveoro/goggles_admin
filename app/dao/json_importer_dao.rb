d7e4ddf455cfa91be3561528ee98420147979e50
Leega <leegaweb@gmail.com>
2019-10-29 17:08:06 +0100

    Data Importer from json DAO draft

--- app/dao/json_importer_dao.rb
+++ app/dao/json_importer_dao.rb
@@ -32,17 +32,115 @@ class JsonImporterDAO
   #-- -------------------------------------------------------------------------
   #++
 
+  # Represents the swimmer data to import
+  #
+  class SwimmerImporterDAO
+
+    # These must be initialized on creation:
+    attr_reader :name, :year, :sex
+
+    # These can be edited later on:
+    attr_accessor :swimmer_id, :badge_id, :results
+
+    # Creates a new instance.
+    #
+    def initialize( name, year, sex, swimmer_id = nil, badge_id = nil )
+      @name = name
+      @year = year
+      @sex  = sex
+
+      # Optional parameters
+      @swimmer_id = swimmer_id
+      @badge_id   = badge_id
+
+      @results = Hash.new()
+    end
+  end
+  #-- -------------------------------------------------------------------------
+  #++
+
+  # Represents the team data to import
+  #
+  class TeamImporterDAO
+
+    # These must be initialized on creation:
+    attr_reader :name
+
+    # These can be edited later on:
+    attr_accessor :affiliation_id, :swimmers
+
+    # Creates a new instance.
+    #
+    def initialize( name, affiliation_id = nil )
+      @name = name
+
+      # Optional parameters
+      @affiliation_id = affiliation_id
+
+      @swimmers = Hash.new()
+    end
+  end
+  #-- -------------------------------------------------------------------------
+  #++
+
+  # Represents the swimmer individual event result
+  #
+  class EventProgramImporterDAO
+
+    # These must be initialized on creation:
+    attr_reader :pool, :sex, :category, :out_of_race, :order
+
+    # Creates a new instance.
+    #
+    def initialize( pool, sex, category, out_of_race = false, order = nil )
+      @pool        = pool
+      @sex         = sex
+      @category    = category
+      @out_of_race = out_of_race
+      @order       = order
+    end
+  end
+  #-- -------------------------------------------------------------------------
+  #++
+
+  # Represents the event data to import
+  #
+  class EventImporterDAO
+
+    # These must be initialized on creation:
+    attr_reader :name
+
+    # These can be edited later on:
+    attr_accessor :event_id, :session_id, :programs
+
+    # Creates a new instance.
+    #
+    def initialize( name, event_id = nil, session_id = nil )
+      @name = name
+
+      # Optional parameters
+      @event_id   = event_id
+      @session_id = session_id
+
+      @programs = Hash.new()
+    end
+  end
+  #-- -------------------------------------------------------------------------
+  #++
 
   # These must be initialized on creation:
   attr_reader :meeting
 
   # These can be edited later on:
-  #attr_accessor
+  attr_accessor :teams, :events
 
   # Creates a new instance.
   #
   def initialize( meeting )
-    @meeting     = meeting
+    @meeting = meeting
+
+    @teams  = Hash.new()
+    @events = Hash.new()
   end
   #-- -------------------------------------------------------------------------
   #++
--- spec/dao/json_importer_dao_spec.rb
+++ spec/dao/json_importer_dao_spec.rb
@@ -1,5 +1,5 @@
 # encoding: utf-8
-
+require 'ffaker'
 require 'rails_helper'
 require 'common/format'
 
@@ -14,21 +14,198 @@ describe JsonImporterDAO, type: :model do
   let(:team_points)         { (rand * 1010).to_i }
   let(:notes)               { 'Nothing to say' }
 
+  let(:swimmer_name)        { "#{FFaker::Name.last_name} #{FFaker::Name.first_name}" }
+  let(:year)                { 1950 + (rand * 50).to_i }
+  let(:sex)                 { GenderType.all.sample.code }
+  let(:swimmer_id)          { (rand * 2000).to_i + 1 }
+  let(:badge_id)            { (rand * 10000).to_i + 1 }
+
+  let(:team_name)           { "#{FFaker::Lorem.word} Swimming Team" }
+  let(:affiliation_id)      { (rand * 1000).to_i + 1 }
+
+  let(:pool)                { PoolType.all.sample.code }
+  let(:category)            { CategoryType.are_not_relays.sample.code }
+  let(:out_of_race)         { ((rand * 10) > 5) }
+  let(:order)               { (rand * 50).to_i + 1 }
+
+  let(:event_name)          { EventType.are_not_relays.sample.code }
+  let(:event_id)            { (rand * 2000).to_i + 1 }
+  let(:session_id)          { (rand * 1000).to_i + 1 }
+
   context "SwimmerResultImporterDAO subelement," do
 
-    subject { JsonImporterDAO::SwimmerResultImporterDAO.new( rank, time_swam, ind_points, team_points, notes ) }
+    subject { JsonImporterDAO::SwimmerResultImporterDAO.new( rank, time_swam ) }
 
     it_behaves_like( "(the existance of a method)", [
       :rank, :time_swam, :ind_points, :team_points, :notes
     ] )
 
-    describe "when initialized with parameters" do
+    describe "when initialized with required parameters" do
       it "returns given values" do
         expect( subject.rank ).to eq( rank )
         expect( subject.time_swam ).to eq( time_swam )
-        expect( subject.ind_points ).to eq( ind_points )
-        expect( subject.team_points ).to eq( team_points )
-        expect( subject.notes ).to eq( notes )
+      end
+      it "returns defaults for optional values" do
+        expect( subject.ind_points ).to eq( 0.00 )
+        expect( subject.team_points ).to eq( 0.00 )
+        expect( subject.notes ).to eq( '' )
+      end
+    end
+
+    describe "when initialized with optional parameters" do
+      it "returns given values" do
+        sri = JsonImporterDAO::SwimmerResultImporterDAO.new( rank, time_swam, ind_points, team_points, notes )
+        expect( sri.ind_points ).to eq( ind_points )
+        expect( sri.team_points ).to eq( team_points )
+        expect( sri.notes ).to eq( notes )
+      end
+    end
+  end
+  #-- -------------------------------------------------------------------------
+  #++
+
+  context "SwimmerImporterDAO subelement," do
+
+    subject { JsonImporterDAO::SwimmerImporterDAO.new( swimmer_name, year, sex ) }
+
+    it_behaves_like( "(the existance of a method)", [
+      :name, :year, :sex,
+      :swimmer_id, :badge_id, :results
+    ] )
+
+    describe "when initialized with required parameters" do
+      it "returns given values" do
+        expect( subject.name ).to eq( swimmer_name )
+        expect( subject.year ).to eq( year )
+        expect( subject.sex ).to eq( sex )
+      end
+      it "returns defaults for optional values" do
+        expect( subject.swimmer_id ).to be_nil
+        expect( subject.badge_id ).to be_nil
+      end
+      it "returns an empty hash for results" do
+        expect( subject.results ).to be_a_kind_of( Hash )
+        expect( subject.results.size ).to eq( 0 )
+      end
+    end
+
+    describe "when initialized with optional parameters" do
+      it "returns given values" do
+        si = JsonImporterDAO::SwimmerImporterDAO.new( swimmer_name, year, sex, swimmer_id, badge_id )
+        expect( si.swimmer_id ).to eq( swimmer_id )
+        expect( si.badge_id ).to eq( badge_id )
+      end
+      it "returns an empty hash for results" do
+        si = JsonImporterDAO::SwimmerImporterDAO.new( swimmer_name, year, sex, swimmer_id, badge_id )
+        expect( si.results ).to be_a_kind_of( Hash )
+        expect( si.results.size ).to eq( 0 )
+      end
+    end
+  end
+  #-- -------------------------------------------------------------------------
+  #++
+
+  context "TeamImporterDAO subelement," do
+
+    subject { JsonImporterDAO::TeamImporterDAO.new( team_name ) }
+
+    it_behaves_like( "(the existance of a method)", [
+      :name,
+      :affiliation_id, :swimmers
+    ] )
+
+    describe "when initialized with required parameters" do
+      it "returns given values" do
+        expect( subject.name ).to eq( team_name )
+      end
+      it "returns defaults for optional values" do
+        expect( subject.affiliation_id ).to be_nil
+      end
+      it "returns an empty hash for results" do
+        expect( subject.swimmers ).to be_a_kind_of( Hash )
+        expect( subject.swimmers.size ).to eq( 0 )
+      end
+    end
+
+    describe "when initialized with optional parameters" do
+      it "returns given values" do
+        ti = JsonImporterDAO::TeamImporterDAO.new( team_name, affiliation_id )
+        expect( ti.affiliation_id ).to eq( affiliation_id )
+      end
+      it "returns an empty hash for results" do
+        ti = JsonImporterDAO::TeamImporterDAO.new( team_name, affiliation_id )
+        expect( ti.swimmers ).to be_a_kind_of( Hash )
+        expect( ti.swimmers.size ).to eq( 0 )
+      end
+    end
+  end
+  #-- -------------------------------------------------------------------------
+  #++
+
+  context "EventProgramImporterDAO subelement," do
+
+    subject { JsonImporterDAO::EventProgramImporterDAO.new( pool, sex, category ) }
+
+    it_behaves_like( "(the existance of a method)", [
+      :pool, :sex, :category, :out_of_race, :order
+    ] )
+
+    describe "when initialized with required parameters" do
+      it "returns given values" do
+        expect( subject.pool ).to eq( pool )
+        expect( subject.sex ).to eq( sex )
+        expect( subject.category ).to eq( category )
+      end
+      it "returns defaults for optional values" do
+        expect( subject.out_of_race ).to eq( false )
+        expect( subject.order ).to be_nil
+      end
+    end
+
+    describe "when initialized with optional parameters" do
+      it "returns given values" do
+        epi = JsonImporterDAO::EventProgramImporterDAO.new( pool, sex, category, out_of_race, order )
+        expect( epi.out_of_race ).to eq( out_of_race )
+        expect( epi.order ).to eq( order )
+      end
+    end
+  end
+  #-- -------------------------------------------------------------------------
+  #++
+
+  context "EventImporterDAO subelement," do
+
+    subject { JsonImporterDAO::EventImporterDAO.new( event_name ) }
+
+    it_behaves_like( "(the existance of a method)", [
+      :name,
+      :event_id, :session_id, :programs
+    ] )
+
+    describe "when initialized with required parameters" do
+      it "returns given values" do
+        expect( subject.name ).to eq( event_name )
+      end
+      it "returns defaults for optional values" do
+        expect( subject.event_id ).to be_nil
+        expect( subject.session_id ).to be_nil
+      end
+      it "returns an empty hash for results" do
+        expect( subject.programs ).to be_a_kind_of( Hash )
+        expect( subject.programs.size ).to eq( 0 )
+      end
+    end
+
+    describe "when initialized with optional parameters" do
+      it "returns given values" do
+        ei = JsonImporterDAO::EventImporterDAO.new( event_name, event_id, session_id )
+        expect( ei.event_id ).to eq( event_id )
+        expect( ei.session_id ).to eq( session_id )
+      end
+      it "returns an empty hash for results" do
+        ei = JsonImporterDAO::EventImporterDAO.new( event_name, event_id, session_id )
+        expect( ei.programs ).to be_a_kind_of( Hash )
+        expect( ei.programs.size ).to eq( 0 )
       end
     end
   end
@@ -40,9 +217,21 @@ describe JsonImporterDAO, type: :model do
     subject { JsonImporterDAO.new( meeting ) }
 
     it_behaves_like( "(the existance of a method)", [
-      :meeting
+      :meeting,
+      :teams, :events
     ] )
 
+    describe "when initialized with required parameters" do
+      it "returns given values" do
+        expect( subject.meeting ).to eq( meeting )
+      end
+      it "returns an empty hash for teams" do
+        expect( subject.teams ).to be_a_kind_of( Hash )
+        expect( subject.teams.size ).to eq( 0 )
+        expect( subject.events ).to be_a_kind_of( Hash )
+        expect( subject.events.size ).to eq( 0 )
+      end
+    end
   end
   #-- -------------------------------------------------------------------------
   #++
